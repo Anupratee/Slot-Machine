e87e6812ade95e821da8569e418c78992fa524ce
Anupratee <anupratee.bharadwaj@gmail.com>
2023-10-05 20:05:24 +0530

    Update project.js
    step two of asking for number of lines completed

--- project.js
+++ project.js
@@ -7,9 +7,9 @@
 //6. give winnings
 //7. play again
 
-//STEP ONE
 const promt = require("prompt-sync")(); //import statement, extra parentheses gives you access to a function
 
+//STEP ONE
 const deposit = () => {
     while (true) {
         const depositAmount = promt("Enter a deposit amount: ");
@@ -24,5 +24,25 @@ const deposit = () => {
     }
 };
 
+//STEP TWO
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = promt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        //check validity of number, the two bars stand for OR
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
 const depositAmount = deposit();
 console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
+console.log(numberOfLines);
+
+
+
