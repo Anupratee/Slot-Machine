4c7a7ea7fb6ae381dbefc3c7a874194567285bae
Anupratee <anupratee.bharadwaj@gmail.com>
2023-10-05 12:00:23 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,4 +1,13 @@
+//steps
+//1. deposit money
+//2. number of lines to bet on
+//3. collect a bet amount
+//4. spin the slot machine
+//5. check if user won
+//6. give winnings
+//7. play again
 
+//STEP ONE
 const promt = require("prompt-sync")(); //import statement, extra parentheses gives you access to a function
 
 const deposit = () => {
